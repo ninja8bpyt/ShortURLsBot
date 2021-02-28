25be45a99fc7b6174cdd1bfe198943a1b9678ce5
ninja8bpyt <78526646+ninja8bpyt@users.noreply.github.com>
2021-02-28 14:40:12 +0530

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -4,12 +4,12 @@
 '''
 
 # Token of the bot. Get it one from https://telegram.me/BotFather
-TOKEN = ""
+TOKEN = "1648439500:AAGRQX1fcj8YddQNKUtOzaC-r0qhuv3HADs"
 # Telegram IDs list of admins of the bot (that can issue /users command)
 ADMINS = []
 
 # Insert absolute path to database file (.db)
-DATABASE_PATH = ""
+DATABASE_PATH = "mongodb+srv://ninja8bpyt:sundrt12@cluster01828.pbim5.mongodb.net/test?retryWrites=true&w=majority"
 
 # Goo.gl API key. Get it one from https://console.developers.google.com/
 GOOGLE_API_KEY = ""
